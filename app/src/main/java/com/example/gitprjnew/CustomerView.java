0697e3f16926df417b2a7672e8a0a189c088af13
ilanakalm <ilana.kalm@gmail.com>
2024-09-13 20:50:12 +0300

    הוספת interface

--- app/src/main/java/com/example/gitprjnew/CustomerView.java
+++ app/src/main/java/com/example/gitprjnew/CustomerView.java
@@ -3,5 +3,6 @@ package com.example.gitprjnew;
 public interface CustomerView {
     void showCustomerDetails(Customer customer);
     void showError(String message);
-
+    void showLoading();
+    void hideLoading();
 }
